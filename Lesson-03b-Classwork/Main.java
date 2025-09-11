01418f54eae0cf2bb5476fabe70ba02b42b7dd96
Leoyu203 <leoy18@nycstudents.net>
2025-09-11 13:28:41 +0000

    save

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.println("Leo Yu");
+    System.out.println("full address");
+    System.out.println("Leo Yu\nfull address");
+   
+    
+  }
+
+  
+}
