6b97d7af93fa89063f006be9f514df59ef6c994a
Leoyu203 <leoy18@nycstudents.net>
2025-09-29 13:34:06 +0000

    www

---
+++ Lesson-08-Activity/Main.java
@@ -0,0 +1,39 @@
+class Main {
+	
+
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+	print("hello");
+	double c
+	}
+	void print(String msg){
+		System.out.println(msg);
+	}
+	double FtoC(double fahrenheit) {
+		C = (fahrenheit - 32) * 5.0 / 9.0;
+		return C;
+	}
+
+	double sphereVolume(double r){
+		V = (4.0/3) * (Math.PI) * Math.pow(r,3);
+		return V;
+	}
+
+	double coneVolume(double r, double h){
+		CV = (Math.PI)*(Math.pow(r,2))*(h/3.0);
+		return CV;
+	}
+
+	double distance(double x1, double y1, double x2, double y2){
+		d = Math.sqrt(Math.pow(x2-x1,2)-Math.pow(y2-y1,2));
+		return d;
+	}
+
+
+  }
+
+  
+ 
