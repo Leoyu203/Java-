dd499374b778bb54ce9a7be5d8fd1a91f16150a3
Leoyu203 <leoy18@nycstudents.net>
2025-10-20 22:56:27 +0000

    svdv

---
+++ Lesson-12-Classwork/Main.java
@@ -0,0 +1,26 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+   
+
+    
+  }
+  String collegeCredit(String s1, String s2){
+    String s1 = collegeCourse;
+    String s2 = ccredit;
+    if (s1.equals(s2))
+      return "true";
+    else
+      return "false";
+  }
+
+  String getCounselor(int class)
+
+ 
+}
